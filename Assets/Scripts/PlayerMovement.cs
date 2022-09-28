5bc98b2c77ad5474c27bae23c6cf95d7d3b1880e
rgo555 <95858479+rgo555@users.noreply.github.com>
2022-09-28 10:56:32 +0200

    programacion_2D

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,38 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// public class PlayerMovement : MonoBehaviour
+ public class PlayerMovement : MonoBehaviour
 {
     //public float speed = 5.5f;
     //private float horizontal;
     //private Transform playerTransform; 
+    private Rigidbody2D rb ;
 
     
-    //void Start()
+    void Start()
     {
         //playerTransform = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     
-    //void Update()
+    void Update()
     {
         //horizontal = Input.GetAxisRaw("Horizontal"); 
 
-        // playerTransform.position  += new Vector3 (horizontal * speed * Time.deltaTime, 0, 0); 
+        //playerTransform.position  += new Vector3 (horizontal * speed * Time.deltaTime, 0, 0); 
 
         //playerTransform.Translate(Vector3.right * horizontal * speed * Time.deltaTime, Space.World);
 
-    }
-}
-
-public class velocity : MonoBehaviour 
-{
-    private Rigidbody2D rb ;
-    private void Awake() {
-        rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate() {
         rb.velocity = new Vector2 (2f ,0f);
     }
+
 }
