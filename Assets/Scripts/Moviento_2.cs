c9314692d6462cc933f27d729d0bacd5ee14901d
rgo555 <95858479+rgo555@users.noreply.github.com>
2022-10-19 19:03:48 +0200

    2D_Jump_Or_Die

--- Assets/Scripts/Moviento_2.cs
+++ Assets/Scripts/Moviento_2.cs
@@ -7,19 +7,46 @@ public class Moviento_2 : MonoBehaviour
     private  Rigidbody2D rb;
     public float speed = 5.5f;
     private float horizontal;
+    public float jumpforce = 7f; 
+    private Animator animator;
+    private Transform playerTransform; 
 
-    private void Awake() {
+    private void Awake()
+    {
         
         rb = GetComponent<Rigidbody2D>();
-
+        animator = GetComponent<Animator>();
+        playerTransform = GetComponent<Transform>();
     }
 
+    void jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(playerTransform.up * jumpforce);
+        }
+    }
    
-    private void FixedUpdate() {
+    private void FixedUpdate() 
+    {
 
         //la velocidad del Rigidbody es un vector que en el eje X, mueves en horizontal dependiendo de la velocidad(multiplica)
-        rb.velocity = new Vector2 (horizontal * speed, 0f);    
+        rb.velocity = new Vector2 (horizontal * speed, 0f);  
 
+        if(horizontal == 0)
+        {
+            animator.SetBool("Run" , false);
+        }  
+        else if (horizontal == 1)
+        {
+            animator.SetBool("Run" , true); 
+            playerTransform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (horizontal == -1)
+        {
+            animator.SetBool("Run" , true); 
+            playerTransform.rotation = Quaternion.Euler(0, -180, 0);
+        }
     }
  
     //Update is called once per frame
@@ -27,6 +54,7 @@ public class Moviento_2 : MonoBehaviour
     {
 
         horizontal = Input.GetAxisRaw("Horizontal");
+        jump();
 
         //playerTransform.position += new Vector3 (1, 0, 0) * horizontal * speed * Time.deltaTime;
 
