6ac9a1cf23dbbc29cd6c0099a2311071da685e37
rgo555 <95858479+rgo555@users.noreply.github.com>
2022-11-09 12:46:01 +0100

    2D-09-11-2022

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -20,7 +20,7 @@ public class CameraShake : MonoBehaviour
         Vector3 originalPos = transform.position;
         float elapsed = 0f; 
 
-        while(elapsed < duration)
+        /*while(elapsed < duration)
         {
             float x = Random.Range(-1f, 1f) * magnitude;
             float y = Random.Range(-1f, 1f) * magnitude;
@@ -29,6 +29,33 @@ public class CameraShake : MonoBehaviour
             elapsed += Time.deltaTime; 
             yield return 0; 
             //56.326026025026
-        }
+        }*/
+
+        /*for(float i = elapsed; i < duration; i += Time.deltaTime)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.position = new Vector3(x + originalPos.x, y + originalPos.y, transform.position.z);
+            yield return 0;
+        }*/
+
+        do
+        {
+            Debug.Log("shake");
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.position = new Vector3(x + originalPos.x, y + originalPos.y, transform.position.z);
+            elapsed += Time.deltaTime; 
+            yield return 0; 
+        }while(elapsed < duration);
+
+        /*GameObject[] vidas; 
+
+        foreach(GameObject vida in vidas)
+        {
+            vida.SetActive(false);
+        }*/
     }
 }
